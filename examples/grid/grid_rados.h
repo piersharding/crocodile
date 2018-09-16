256731e1ceebfdfac2ce8c664c9d987f8c4eba5b
Piers Harding <piers@catalyst.net.nz>
2018-09-17 10:58:32 +1200

    io_benchmark: add rados version of hdf5

---
+++ examples/grid/grid_rados.h
@@ -0,0 +1,16 @@
+
+#ifndef GRID_RADOS_H
+#define GRID_RADOS_H
+
+// rados configuration
+hid_t fapl;
+
+#ifdef GRID_ON_RADOS
+
+// define constants for connection to kubernetes pool as kubernetes user
+const char *grid_rados_pool = "kubernetes";
+const char *grid_rados_user = "kubernetes";
+
+#endif // GRID_ON_RADOS
+
+#endif // GRID_RADOS_H
